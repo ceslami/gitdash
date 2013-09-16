01d96740034534672c1c49f973fdf727bf30b2e9
Cyrus Eslami <ceslami@gmail.com>
2013-09-16 04:54:15 -0400

    team statistics

--- app/index.php
+++ app/index.php
@@ -21,6 +21,11 @@
 
     $app->get('/app/?', function() use ($app, $auth_url) {
         $token = isset($_COOKIE['access_token']) ? $_COOKIE['access_token'] : "";
+        $users = array();
+        $pull_requests_count = 0;
+        $repo_count = 0;
+        $unique_users_count = 0;
+        $old_posts = 0;
 
         $repos = get_data('https://api.github.com/orgs/Betterment/repos?type=private');
 
@@ -29,7 +34,11 @@
                 $pull_requests = get_data('https://api.github.com/repos/Betterment/'.$repo->name.'/pulls');
 
                 if(count($pull_requests)) {
-                    echo "<h3><a target='_blank' href='".$repo->html_url."/pulls'>".$repo->name."</a> &middot; (".count($pull_requests).")</h3>";
+                    echo "<h3>".
+                            "<a target='_blank' style='float:left' href='".$repo->html_url."/pulls'>".$repo->name."</a>".
+                            // "<span style='float:right'>".count($pull_requests)." open requests</span>".
+                            // "<div style='clear:both'></div>".
+                         "</h3>";
                     echo "<table>";
                         foreach($pull_requests as $pr) {
                             $pull_request = get_data('https://api.github.com/repos/Betterment/'.$repo->name.'/pulls/'.$pr->number);
@@ -37,6 +46,14 @@
                             $date_parts = explode('T', $pull_request->created_at);
                             $days_ago = days_ago($date_parts[0]);
                             $days_ago_str = $days_ago ? $days_ago.' days ago' : '<24 hours ago';
+
+                            if($days_ago >= 5) {
+                                $old_posts++;
+                            }
+
+                            $users[] = $pull_request->user->login;
+                            $pull_requests_count++;
+                            $repo_count++;
 
                             echo "<tr>";
                                 echo "<td style='width:50%;' valign='top'><a target='_blank' href='".$pull_request->html_url."'>".$pull_request->title."</a></td>".
@@ -51,6 +68,16 @@
                     echo "<hr style='opacity:.3'>";
                 }
             }
+        echo "</div>";
+
+        $unique_users_count = count(array_unique($users));
+        echo "<div style='float:right;width:40%;'>";
+            echo "<div style='margin:20px'>";
+                echo "<div style='font-size:18px;margin:0 0 5px'><span style='display:inline-block;width:40px'>".$repo_count."</span> repositories with open requests</div>";
+                echo "<div style='font-size:18px;margin:0 0 5px'><span style='display:inline-block;width:40px'>".$pull_requests_count."</span> open pull requests</div>";
+                echo "<div style='font-size:18px;margin:0 0 5px'><span style='display:inline-block;width:40px'>".$unique_users_count."</span> devs with open requests</div>";
+                echo "<div style='font-size:18px;margin:0 0 5px'><span style='display:inline-block;width:40px'>".$old_posts."</span> pull requests >5 days old</div>";
+            echo "</div>";
         echo "</div>";
     });
 
