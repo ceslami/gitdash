0bf127bc261270e899cd229eff9d97fed9abd487
Cyrus Eslami <ceslami@gmail.com>
2013-09-22 16:47:51 -0400

    Reorg frontend assets

--- app/templates/index.php
+++ app/templates/index.php
@@ -1,9 +1,9 @@
 <html>
     <head>
         <link href='http://fonts.googleapis.com/css?family=Lato:100,300,400,700,900,300italic,400italic,700italic' rel='stylesheet' type='text/css'>
-        <link href='/css/jquery.easy-pie-chart.css' rel='stylesheet' type='text/css'>
+        <link href='/assets/css/jquery.easy-pie-chart.css' rel='stylesheet' type='text/css'>
         <link href='//netdna.bootstrapcdn.com/font-awesome/3.2.1/css/font-awesome.min.css' rel='stylesheet' type='text/css'>
-        <link href="/css/style.css" rel="stylesheet" type="text/css">
+        <link href="/assets/css/style.css" rel="stylesheet" type="text/css">
     </head>
     <body>
         <header>
@@ -30,30 +30,30 @@
         <?php include 'underscore-templates.php'; ?>
 
         <!-- TEMPLATES -->
-        <script type="text/javascript" src="/js/vendor/local-storage.js"></script>
-        <script type="text/javascript" src="/js/vendor/jquery.min.js"></script>
-        <script type="text/javascript" src="/js/vendor/jquery-ui-custom.js"></script>
-        <script type="text/javascript" src="/js/vendor/jquery.easy-pie-chart.js"></script>
-        <script type="text/javascript" src="/js/vendor/mustache.js"></script>
-        <script type="text/javascript" src="/js/vendor/underscore.js"></script>
-        <script type="text/javascript" src="/js/vendor/backbone.js"></script>
-        <script type="text/javascript" src="/js/vendor/backbone.babysitter.js"></script>
-        <script type="text/javascript" src="/js/vendor/backbone.wreqr.js"></script>
-        <script type="text/javascript" src="/js/vendor/marionette.js"></script>
+        <script type="text/javascript" src="/assets/js/vendor/local-storage.js"></script>
+        <script type="text/javascript" src="/assets/js/vendor/jquery.min.js"></script>
+        <script type="text/javascript" src="/assets/js/vendor/jquery-ui-custom.js"></script>
+        <script type="text/javascript" src="/assets/js/vendor/jquery.easy-pie-chart.js"></script>
+        <script type="text/javascript" src="/assets/js/vendor/mustache.js"></script>
+        <script type="text/javascript" src="/assets/js/vendor/underscore.js"></script>
+        <script type="text/javascript" src="/assets/js/vendor/backbone.js"></script>
+        <script type="text/javascript" src="/assets/js/vendor/backbone.babysitter.js"></script>
+        <script type="text/javascript" src="/assets/js/vendor/backbone.wreqr.js"></script>
+        <script type="text/javascript" src="/assets/js/vendor/marionette.js"></script>
 
-        <script type="text/javascript" src="/js/src/app.js"></script>
-        <script type="text/javascript" src="/js/src/models/GithubUser.js"></script>
-        <script type="text/javascript" src="/js/src/models/PullRequest.js"></script>
-        <script type="text/javascript" src="/js/src/models/PullRequests.js"></script>
-        <script type="text/javascript" src="/js/src/models/Settings.js"></script>
-        <script type="text/javascript" src="/js/src/views/PullRequestItemView.js"></script>
-        <script type="text/javascript" src="/js/src/views/HomePullRequestsView.js"></script>
-        <script type="text/javascript" src="/js/src/views/HomeHeadsUpView.js"></script>
-        <script type="text/javascript" src="/js/src/views/HomeLayout.js"></script>
-        <script type="text/javascript" src="/js/src/views/FooterLayout.js"></script>
-        <script type="text/javascript" src="/js/src/views/SettingsLayout.js"></script>
-        <script type="text/javascript" src="/js/src/Router.js"></script>
+        <script type="text/javascript" src="/assets/js/src/app.js"></script>
+        <script type="text/javascript" src="/assets/js/src/models/GithubUser.js"></script>
+        <script type="text/javascript" src="/assets/js/src/models/PullRequest.js"></script>
+        <script type="text/javascript" src="/assets/js/src/models/PullRequests.js"></script>
+        <script type="text/javascript" src="/assets/js/src/models/Settings.js"></script>
+        <script type="text/javascript" src="/assets/js/src/views/PullRequestItemView.js"></script>
+        <script type="text/javascript" src="/assets/js/src/views/HomePullRequestsView.js"></script>
+        <script type="text/javascript" src="/assets/js/src/views/HomeHeadsUpView.js"></script>
+        <script type="text/javascript" src="/assets/js/src/views/HomeLayout.js"></script>
+        <script type="text/javascript" src="/assets/js/src/views/FooterLayout.js"></script>
+        <script type="text/javascript" src="/assets/js/src/views/SettingsLayout.js"></script>
+        <script type="text/javascript" src="/assets/js/src/Router.js"></script>
 
-        <script type="text/javascript" src="/js/src/init.js"></script>
+        <script type="text/javascript" src="/assets/js/src/init.js"></script>
     </body>
 </html>
